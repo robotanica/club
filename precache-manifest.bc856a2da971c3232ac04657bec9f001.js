a87d41d3ed0964e84a7548894187a794fb49dc27
Gu Andres <andresgu@gmail.com>
2019-11-13 20:52:02 -0600

    app deploy

---
+++ precache-manifest.bc856a2da971c3232ac04657bec9f001.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad434f17c1d7412fe639",
+    "url": "/css/app.5e61afc4.css"
+  },
+  {
+    "revision": "11000b000084aa3e04fb76577b0929b5",
+    "url": "/img/logo.11000b00.png"
+  },
+  {
+    "revision": "947d64474b2bfe1db30bcaca51db47be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1b261cd07c007d842fe",
+    "url": "/js/about.92ee3899.js"
+  },
+  {
+    "revision": "ad434f17c1d7412fe639",
+    "url": "/js/app.956fb318.js"
+  },
+  {
+    "revision": "dba97efa7588c7bcfe83",
+    "url": "/js/chunk-vendors.54c46db6.js"
+  },
+  {
+    "revision": "75d414dfa397d2bdeda8b5b44de09909",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
